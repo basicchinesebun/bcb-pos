1495a733b416e125fc34a0cbb0cff027034fc598
soutthanou <soutthanou12@gmail.com>
2026-04-29 10:01:19 +0700

    fix: remove conflicting server exports from client pages

--- src/app/order/page.js
+++ src/app/order/page.js
@@ -1,8 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 import { useState, useEffect, useCallback } from 'react'
 import { supabase } from '../../lib/supabase'
 import ContactSection from '../../components/ContactSection'
